29939db3f527d36a08902a20f3992662cdeebf85
acnologia <shrikanthbudya@gmail.com>
2018-04-01 14:16:38 +0530

    added the lerping functionality.

---
+++ scripts/script.js
@@ -0,0 +1,75 @@
+var can = document.getElementById("can");
+var ctx = can.getContext("2d");
+
+const radius = 50;
+
+var angle = 0, angle1 = 0;
+
+function resize() {
+    can.width = window.innerWidth;
+    can.height = window.innerHeight;
+}
+function lerp(a, b, t) {
+    return (1 - t) * a + t * b;
+}
+
+function x(offset, angle) {
+    return offset + Math.sin(angle) * radius;
+}
+function y(offset, angle) {
+    return offset + Math.cos(angle) * radius;
+}
+
+function draw() {
+    ctx.clearRect(0, 0, can.width, can.height);
+
+    ctx.fillStyle = "#000";
+
+    var a = {
+        x: x(can.width * 0.25, angle),
+        y: y(can.height / 2, angle)
+    }
+    var b = {
+        x: x(can.width * 0.75, angle1),
+        y: y(can.height / 2, angle1)
+    }
+    ctx.beginPath();
+    ctx.arc(a.x, a.y, 3, 0, Math.PI * 2);
+
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(b.x, b.y, 3, 0, Math.PI * 2);
+
+    ctx.closePath();
+    ctx.fill();
+    var total = 100;
+    for (var i = 0; i < total; ++i) {
+        ctx.beginPath();
+        var between = i / total;
+        var temp = {
+            x: lerp(
+                x(can.width * 0.25, angle * (1 - between)),
+                x(can.width * 0.75, angle1 * between),
+                between
+            ),
+            y: lerp(
+                y(can.height / 2, angle * (1 - between)),
+                y(can.height / 2, angle1 * between),
+                between
+            )
+        };
+        ctx.arc(temp.x, temp.y, 1, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+
+    angle += Math.PI / 60;
+    angle1 -= Math.PI / 60;
+    requestAnimationFrame(draw);
+}
+window.onresize = resize;
+resize();
+draw();
